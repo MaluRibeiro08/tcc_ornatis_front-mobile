b6e9091b3890938fded1bb6ae3f4f76c62af3b1a
AriellyAngelo <sara.silvadefaria@hotmail.com>
2022-03-25 10:52:11 -0300

    adicionando uma nova pasta do projeto

---
+++ TESTE-TCC/app/src/main/java/com/example/teste_tcc/CadastroPerfilAdministrador.java
@@ -0,0 +1,16 @@
+package com.example.teste_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teste_tcc.R;
+
+public class CadastroPerfilAdministrador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_perfil_administrador);
+    }
+}
