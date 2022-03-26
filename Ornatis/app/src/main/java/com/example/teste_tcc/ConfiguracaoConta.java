1f675de9aff9734468466a12357efc5f0392453a
sarah1510 <sara.silvadefaria@hotmail.com>
2022-03-26 17:51:05 -0300

    atualizando o projeto da pasta ORNATIS

---
+++ Ornatis/app/src/main/java/com/example/teste_tcc/ConfiguracaoConta.java
@@ -0,0 +1,14 @@
+package com.example.teste_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfiguracaoConta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracao_conta);
+    }
+}
