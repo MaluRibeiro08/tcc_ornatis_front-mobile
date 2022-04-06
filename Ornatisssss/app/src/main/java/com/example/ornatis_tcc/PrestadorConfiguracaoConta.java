357119255c6688cee232d7a26e78b1c61651e141
AriellyAngelo <sara.silvadefaria@hotmail.com>
2022-04-06 10:09:16 -0300

    fazendo aparecer os containers na tela de configuração da conta

--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/PrestadorConfiguracaoConta.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/PrestadorConfiguracaoConta.java
@@ -3,12 +3,45 @@ package com.example.ornatis_tcc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class PrestadorConfiguracaoConta extends AppCompatActivity {
+
+    private TextView text_view_perfil_estabelecimento;
+    private LinearLayout container_perfil_estabelecimento;
+
+    private TextView tv_perfil_adm;
+    private LinearLayout container_perfil_adm;
+
+    private TextView tv_dados_localizacao;
+    private LinearLayout containcontainer_dados_localizacaoer_perfil_adm;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestador_configuracao_conta);
+
+        text_view_perfil_estabelecimento = findViewById(R.id.text_view_perfil_estabelecimento);
+        container_perfil_estabelecimento = findViewById(R.id.container_perfil_estabelecimento);
+
+        text_view_perfil_estabelecimento.setOnClickListener(view -> {
+
+            if(container_perfil_estabelecimento.getVisibility() == View.VISIBLE)
+            {
+                container_perfil_estabelecimento.setVisibility(View.GONE);
+            }
+            else
+            {
+                container_perfil_estabelecimento.setVisibility(View.VISIBLE);
+            }
+
+
+
+        });
     }
 }
