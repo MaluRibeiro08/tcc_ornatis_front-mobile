5b309bb2f3f54eb639b8dfdaee6899991e3708bd
sarah1510 <sara.silvadefaria@hotmail.com>
2022-04-23 13:55:22 -0300

    ajustando icones do bottom navigation bar

--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/HomePrestador.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/HomePrestador.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class HomePrestador extends AppCompatActivity {
 
-    private TextView engrenagem;
+    private ImageView engrenagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
