2fc10403d5b68b02e7a2f2db75734266999745d6
sarah1510 <sara.silvadefaria@hotmail.com>
2022-04-25 01:15:31 -0300

    processo da tela de listagem de funcionários (não finalizado)

---
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/PrestadorListagemFuncionarios.java
@@ -0,0 +1,14 @@
+package com.example.ornatis_tcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrestadorListagemFuncionarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prestador_listagem_funcionarios);
+    }
+}
