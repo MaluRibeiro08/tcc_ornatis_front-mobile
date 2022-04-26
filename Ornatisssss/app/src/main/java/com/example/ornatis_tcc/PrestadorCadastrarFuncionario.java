def690414b24ed4e7f6e114edbad67a0ee964d3d
AriellyAngelo <sara.silvadefaria@hotmail.com>
2022-04-26 11:23:04 -0300

    telas SP2 (relacionadas aos funcionários)

--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/PrestadorCadastrarFuncionario.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/PrestadorCadastrarFuncionario.java
@@ -1,8 +1,11 @@
 package com.example.ornatis_tcc;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class PrestadorCadastrarFuncionario extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class PrestadorCadastrarFuncionario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestador_cadastrar_funcionario);
+
+        //COLOCANDO A SETA DE VOLTAR NA PARTE SUPERIOR DA TELA
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                this.finish();
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/PrestadorListagemFuncionarios.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/PrestadorListagemFuncionarios.java
@@ -2,13 +2,25 @@ package com.example.ornatis_tcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PrestadorListagemFuncionarios extends AppCompatActivity {
+
+    private TextView adicionando_novo_funcionario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestador_listagem_funcionarios);
+
+        adicionando_novo_funcionario = findViewById(R.id.adicionando_novo_funcionario);
+
+        //MUDAR DE TELA AO CLICAR NO +
+//        adicionando_novo_funcionario.setOnClickListener(view -> {
+//
+//        });
+
     }
 }
