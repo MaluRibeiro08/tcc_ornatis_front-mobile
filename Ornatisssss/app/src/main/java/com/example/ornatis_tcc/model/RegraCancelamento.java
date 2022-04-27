5ec74f0894899b8473e8bf2e5cd9f51b9bcaef6a
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-04-27 09:13:53 -0300

    Criação da classe regra de cancelamento

---
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/model/RegraCancelamento.java
@@ -0,0 +1,50 @@
+package com.example.ornatis_tcc.model;
+
+public class RegraCancelamento
+{
+    private int acima_cem;
+    private int porcentagem_sobre_valor_servico;
+    private int horas_tolerancia;
+
+    //CONSTRUTOR VAZIO
+    public RegraCancelamento() { }
+
+
+    //CONSTRUTOR COM OS ATRIBUTOS
+    public RegraCancelamento(int acima_cem, int porcentagem_sobre_valor_servico, int horas_tolerancia)
+    {
+        this.acima_cem = acima_cem;
+        this.porcentagem_sobre_valor_servico = porcentagem_sobre_valor_servico;
+        this.horas_tolerancia = horas_tolerancia;
+    }
+
+    public int getAcima_cem()
+    {
+        return acima_cem;
+    }
+
+    public void setAcima_cem(int acima_cem)
+    {
+        this.acima_cem = acima_cem;
+    }
+
+    public int getPorcentagem_sobre_valor_servico()
+    {
+        return porcentagem_sobre_valor_servico;
+    }
+
+    public void setPorcentagem_sobre_valor_servico(int porcentagem_sobre_valor_servico)
+    {
+        this.porcentagem_sobre_valor_servico = porcentagem_sobre_valor_servico;
+    }
+
+    public int getHoras_tolerancia()
+    {
+        return horas_tolerancia;
+    }
+
+    public void setHoras_tolerancia(int horas_tolerancia)
+    {
+        this.horas_tolerancia = horas_tolerancia;
+    }
+}
