be4f55539019e373f776e57598d8cc1e967f7d1d
sarah1510 <sara.silvadefaria@hotmail.com>
2022-05-01 22:57:08 -0300

    adicionando noas telas + ajustes no menu bottom nav

---
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/UI/conta_administradora/servico/PrestadorCadastroServicos.java
@@ -0,0 +1,16 @@
+package com.example.ornatis_tcc.UI.conta_administradora.servico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ornatis_tcc.R;
+
+public class PrestadorCadastroServicos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prestador_cadastro_servicos);
+    }
+}
