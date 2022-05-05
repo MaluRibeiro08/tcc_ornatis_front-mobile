a553760f1c78c50972c564c8e3a9128578a32f04
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-05-05 11:23:08 -0300

    Mudanca envio do horario

--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/model/DiaUtil.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/model/DiaUtil.java
@@ -4,14 +4,19 @@ import java.time.LocalTime;
 
 public class DiaUtil {
     private int id_dia_semana;
-    private LocalTime hora_inicio;
-    private LocalTime hora_termino;
+    private LocalTime hora_inicio_localtime;
+    private String hora_inicio;
+    private LocalTime hora_termino_localtime;
+    private String hora_termino;
 
-    public DiaUtil(int id_dia_semana, LocalTime hora_inicio, LocalTime hora_termino)
+    public DiaUtil(int id_dia_semana, LocalTime hora_inicio_localtime, LocalTime hora_termino_localtime)
     {
         this.id_dia_semana = id_dia_semana;
-        this.hora_inicio = hora_inicio;
-        this.hora_termino = hora_termino;
+        this.hora_inicio_localtime = hora_inicio_localtime;
+        this.hora_termino_localtime = hora_termino_localtime;
+
+        this.hora_inicio = hora_inicio_localtime.toString();
+        this.hora_termino = hora_termino_localtime.toString();
     }
 
     public int getId_dia_semana() {
@@ -22,19 +27,19 @@ public class DiaUtil {
         this.id_dia_semana = id_dia_semana;
     }
 
-    public LocalTime getHora_inicio() {
-        return hora_inicio;
+    public LocalTime getHora_inicio_localtime() {
+        return hora_inicio_localtime;
     }
 
-    public void setHora_inicio(LocalTime hora_inicio) {
-        this.hora_inicio = hora_inicio;
+    public void setHora_inicio_localtime(LocalTime hora_inicio_localtime) {
+        this.hora_inicio_localtime = hora_inicio_localtime;
     }
 
-    public LocalTime getHora_termino() {
-        return hora_termino;
+    public LocalTime getHora_termino_localtime() {
+        return hora_termino_localtime;
     }
 
-    public void setHora_termino(LocalTime hora_termino) {
-        this.hora_termino = hora_termino;
+    public void setHora_termino_localtime(LocalTime hora_termino_localtime) {
+        this.hora_termino_localtime = hora_termino_localtime;
     }
 }
