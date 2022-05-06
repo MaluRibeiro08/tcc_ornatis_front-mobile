e1369148b37486a313964a84273ec78c02619e91
AriellyAngelo <sara.silvadefaria@hotmail.com>
2022-05-06 11:23:49 -0300

    criando classe adapter

--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/UI/conta_administradora/funcionario/PrestadorListagemFuncionarios.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/UI/conta_administradora/funcionario/PrestadorListagemFuncionarios.java
@@ -1,17 +1,26 @@
 package com.example.ornatis_tcc.UI.conta_administradora.funcionario;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ornatis_tcc.R;
 import com.example.ornatis_tcc.model.Funcionarios;
+import com.example.ornatis_tcc.model.Item;
 import com.example.ornatis_tcc.remote.APIUtil;
 import com.example.ornatis_tcc.remote.RouterInterface;
 
@@ -38,51 +47,108 @@ public class PrestadorListagemFuncionarios extends AppCompatActivity {
 
         routerInterface = APIUtil.getEmpresaInterface();
 
-//        Call<Funcionarios> call = routerInterface.getFuncionario();
+        Call<ArrayList<Funcionarios>> call = routerInterface.getFuncionario();
 
-//        call.enqueue(new Callback<Funcionarios>() {
-//            @Override
-//            public void onResponse(Call<Funcionarios> call, Response<Funcionarios> response) {
-//                if (response.isSuccessful()){
-//                    List<Funcionarios> funcionarios = new ArrayList<>();
+        call.enqueue(new Callback<ArrayList<Funcionarios>>() {
+                         @Override
+                         public void onResponse(Call<ArrayList<Funcionarios>> call, Response<ArrayList<Funcionarios>> response) {
+                             if (response.isSuccessful()){
+                                 ArrayList<Funcionarios> funcionarios = new ArrayList<>();
+
+                                 //RECEBE OS DADOS DA API
+                                 ArrayList<Funcionarios> arrayList = new ArrayList<Funcionarios>();
+                                 arrayList = response.body();
+
+                                 for (int i = 0; i < arrayList.size(); i++){
+                                     funcionarios.add(new Funcionarios());
+                                     //o que ser colocado dentro desse ultimo parenteses de Funcionarios()
+                                 }
+
+                                 RecyclerView recyclerView = findViewById(R.id.recyclerViewFuncionarios);
+//                                 recyclerView.setAdapter(new FuncionarioAdapter()); //funcionarios -> colocar dentro desse parenteses?
+                             }
+                         }
+
+                         @Override
+                         public void onFailure(Call<ArrayList<Funcionarios>> call, Throwable t) {
+
+                         }
+                     });
+
+
+        //        MUDAR DE TELA AO CLICAR NO +
+        //        adicionando_novo_funcionario.setOnClickListener(view -> {
+        //            startActivity(new Intent(PrestadorListagemFuncionarios.this, PrestadorCadastrarFuncionario.class));
+        //        });
+
+
+    }//fim do método onCreate
+
+
+
+    /** ADAPTER - RECYCLERVIEW **/
+//    private class FuncionarioAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 //
-//                    List<Funcionarios> list = new ArrayList<>();
-////                    list = response.body();
+//        ArrayList<Funcionarios> funcionario;
 //
-////                    for(int i = 0; i < list.size(); i++){
-////                        funcionarios.add(new Funcionarios(0, list.get(i)));
-////                    }
+//        public FuncionarioAdapter(ArrayList<Funcionarios> funcionario) {
+//            this.funcionario = funcionario;
+//        }
 //
-//                    RecyclerView recyclerView = findViewById(R.id.recyclerView_funcionarios);
-////                    recyclerView.setAdapter(new FuncionariosAdapter(funcionarios));
+//        @NonNull
+//        @Override
+//        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 //
+//            return new FuncionarioAdapter.FuncionarioViewHolder(
+//                    LayoutInflater.from(
+//                            parent.getContext()).inflate(
+//                                R.layout.item_container_listagem_funcionario,
+//                                parent,
+//                                false
+//                            ));
+//        }
 //
-//                }
+//        @Override
+//        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+            /** DADOS DE FUNCIONÁRIO **/
+//            if (getItemViewType(position) == 0){
+//                Funcionarios funcionarios = (Funcionarios) funcionario.get(position).getObject();
+//                ((FuncionarioAdapter.FuncionarioViewHolder) holder).setFuncionarioData(funcioanrio);
 //            }
+//        }
 //
-//            @Override
-//            public void onFailure(Call<Funcionarios> call, Throwable t) {
-//
+//        @Override
+//        public int getItemCount() {
+//            return 0;
+//        }
+
+
+//        class FuncionarioViewHolder extends RecyclerView.ViewHolder {
+//            public FuncionarioViewHolder(View inflate) {
+////                super();
 //            }
-//        });
+//        } /** FIM DA CLASSE VIEWHOLDER **/
+//
+//    } /****** FIM DA CLASSE ADAPTER ******/
+
+
+
+//        public FuncionarioAdapter(ArrayList<Funcionarios> funcionarios){
+//            this.funcionarios = funcionarios;
+//        }
 
 
 
 
 
-
-
-
-
-
-//        MUDAR DE TELA AO CLICAR NO +
-//        adicionando_novo_funcionario.setOnClickListener(view -> {
-//            startActivity(new Intent(PrestadorListagemFuncionarios.this, PrestadorCadastrarFuncionario.class));
-//        });
-
-
-
-    }
+//    }
 
 
 }
+
+
+
+
+
+
--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/remote/RouterInterface.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/remote/RouterInterface.java
@@ -3,6 +3,7 @@ package com.example.ornatis_tcc.remote;
 import com.example.ornatis_tcc.model.ContaAdministradora;
 import com.example.ornatis_tcc.model.Funcionarios;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -27,7 +28,7 @@ public interface RouterInterface {
 
     //LISTAGEM DE FUNCIONÁRIOS
     @GET("/tcc_ornatis_back-end/api-ornatis/rotas/adm/")
-    Call<Funcionarios>getFuncionario();
+    Call<ArrayList<Funcionarios>>getFuncionario();
 
     //LISTAGEM DOS DADOS DO FUNCIONÁRIO
     @GET("/tcc_ornatis_back-end/api-ornatis/rotas/adm/")
