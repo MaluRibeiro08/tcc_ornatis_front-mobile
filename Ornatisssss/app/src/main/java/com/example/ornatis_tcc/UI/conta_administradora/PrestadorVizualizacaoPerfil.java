70d61aaf2fc46b998e29d02d19434aa37c8052ec
Maria Luiza <maluribeiro08@gmail.com>
2022-05-12 16:43:19 -0300

    Atribuindo dados aos campos - ainda nao finalizado

--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/UI/conta_administradora/PrestadorVizualizacaoPerfil.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/UI/conta_administradora/PrestadorVizualizacaoPerfil.java
@@ -13,9 +13,6 @@ import com.example.ornatis_tcc.model.ContaAdministradora;
 import com.example.ornatis_tcc.remote.APIUtil;
 import com.example.ornatis_tcc.remote.RouterInterface;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -23,21 +20,49 @@ import retrofit2.Response;
 public class PrestadorVizualizacaoPerfil extends AppCompatActivity {
 
     RouterInterface routerInterface;
-    TextView tv_aba_inicio;
-    TextView tv_aba_servicos;
-    TextView tv_aba_produtos;
-    TextView tv_aba_feedback;
-    LinearLayout container_inicio;
-    LinearLayout container_servicos;
-    LinearLayout container_produtos;
-    LinearLayout container_feedbacks;
-    int id_empresa =33;
+
+    //NAVEGACAO E ABAS
+        TextView tv_aba_inicio;
+        TextView tv_aba_servicos;
+        TextView tv_aba_produtos;
+        TextView tv_aba_feedback;
+        LinearLayout container_inicio;
+        LinearLayout container_servicos;
+        LinearLayout container_produtos;
+        LinearLayout container_feedbacks;
+
+    //ABA INICIO
+        TextView tv_nome_do_negocio_perfil;
+        TextView tv_biografia_perfil;
+        TextView tv_contato_perfil;
+        TextView tv_endereco_perfil;
+        TextView tv_instagram_salao_perfil;
+        TextView tv_facebook_salao_perfil;
+        LinearLayout ln_container_formas_pagamento_perfil;
+
+
+
+
+
+
+//    ContaAdministradora contaAdministradora;
+    int id_empresa =30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestador_vizualizacao_perfil);
+
+        //REFERENCIAS AOS ELEMENTOS GRAFICOS
+            //ABA INICIAL
+                tv_nome_do_negocio_perfil = findViewById(R.id.tv_nome_do_negocio_perfil);
+                tv_biografia_perfil= findViewById(R.id.tv_biografia_perfil);
+                tv_contato_perfil= findViewById(R.id.tv_contato_perfil);
+                tv_endereco_perfil= findViewById(R.id.tv_endereco_perfil);
+                tv_instagram_salao_perfil= findViewById(R.id.tv_instagram_salao_perfil);
+                tv_facebook_salao_perfil= findViewById(R.id.tv_facebook_salao_perfil);
+                ln_container_formas_pagamento_perfil = findViewById(R.id.ln_container_formas_pagamento_perfil);
 
         //NAVEGAÇÃO ENTRE AS SECOES
             //ATRIBUINDO OS ELEMENTOS GRÁFICOS À VARIAVEL
@@ -73,9 +98,7 @@ public class PrestadorVizualizacaoPerfil extends AppCompatActivity {
                         }
                 );
         //BUSCAR OS DADOS E PREENCHER SECAO 'INICIO'
-
-        ContaAdministradora contaAdministradora = getDadosPerfilEstabelecimento(id_empresa);
-
+            getDadosPerfilEstabelecimento(id_empresa);
     }
 
     public ContaAdministradora getDadosPerfilEstabelecimento (int id_empresa)
@@ -91,25 +114,23 @@ public class PrestadorVizualizacaoPerfil extends AppCompatActivity {
                         {
                             Log.d("dados_perfil", "onResponse: deu certo ");
 
-                            ContaAdministradora conta = new ContaAdministradora();
-
-                            Log.d("dados_perfil", "onResponse: infos: " + response.body().getCep());
+                            ContaAdministradora conta = response.body();
 
                             conta = response.body();
 
-                            String teste = conta.getNome_fantasia();
+                           preencherInformacoesAbaInicial(conta);
 
-                            Log.d("dados_perfil", "onResponse: " + teste);
-                            Log.d("dados_perfil", "onResponse: olho o cep aí " + response.body().getCep());
-                            Log.d("dados_perfil", "onResponse: olho o funcionamento aí " + response.body().getDados_funcionamento().get(0));
-                            Log.d("dados_perfil", "onResponse: olha as formas de pagamento aí " + response.body().getDados_formas_pagamento().get(0));
-                            Log.d("dados_perfil", "onResponse: olha as observacoes de pagamento aí " + response.body().getObservacoes_pagamento());
-//                            Log.d("dados_perfil", "onResponse: olha as taxas aí" + response.body().getDados_taxa_cancelamento().get(0));
-                            Log.d("dados_perfil", "onResponse: olha taxa unica aí " + response.body().getTaxa_unica_cancelamento());
+                            //DEBUGANDO
+                                //Log.d("dados_perfil", "onResponse: infos: " + response.body().getCep());
+                                //Log.d("dados_perfil", "onResponse: nome fantasia: " + conta.getNome_fantasia());
+                                //Log.d("dados_perfil", "onResponse: olho o cep aí " + conta.getCep());
+                                //Log.d("dados_perfil", "onResponse: olho o funcionamento aí " + conta.getDados_funcionamento().get(0));
+                                //Log.d("dados_perfil", "onResponse: olha as formas de pagamento aí " + conta.getDados_formas_pagamento().get(0));
+                                //Log.d("dados_perfil", "onResponse: olha as observacoes de pagamento aí " + conta.getObservacoes_pagamento());
+                                //Log.d("dados_perfil", "onResponse: olha as taxas aí" + response.body().getDados_taxa_cancelamento().get(0));
+                                //Log.d("dados_perfil", "onResponse: olha taxa unica aí " + conta.getTaxa_unica_cancelamento());
 
                         }
-                        Log.d("dados_perfil", "onResponse: "+ response.message());
-
                     }
 
                     @Override
@@ -123,6 +144,7 @@ public class PrestadorVizualizacaoPerfil extends AppCompatActivity {
 
         return contaAdministradora;
     }
+
     public void trocarVisualizacaoAbas(LinearLayout linearASerVisualizado)
     {
         LinearLayout [] containers = new LinearLayout[] {container_inicio, container_servicos, container_produtos, container_feedbacks};
@@ -135,5 +157,67 @@ public class PrestadorVizualizacaoPerfil extends AppCompatActivity {
         }
 
         linearASerVisualizado.setVisibility(View.VISIBLE);
+    }
+
+    public void preencherInformacoesAbaInicial(ContaAdministradora contaAdministradora)
+    {
+
+        Log.d("DADOS_CONTA", "preencherInformacoesPerfilSalao: " + contaAdministradora.getNome_fantasia());
+
+        String endereco_salao = contaAdministradora.getRua() + ", " +
+                                                    contaAdministradora.getNumero_rua() + ". " +
+                                                    contaAdministradora.getBairro() + ", " +
+                                                    contaAdministradora.getNome_cidade() + "/" +
+                                                    contaAdministradora.getSigla_estado() + ". " +
+                                                    contaAdministradora.getCep() + ".";
+        tv_nome_do_negocio_perfil.setText(contaAdministradora.getNome_fantasia());
+        tv_biografia_perfil.setText(contaAdministradora.getBiografia());
+        tv_contato_perfil.setText(contaAdministradora.getTelefone());
+        tv_endereco_perfil.setText(endereco_salao);
+
+        //REDES SOCIAIS
+            if (contaAdministradora.getNome_usuario_instagram() == null)
+            {
+
+                tv_instagram_salao_perfil.setText("Não cadastrado :/");
+            }
+            else
+            {
+                tv_instagram_salao_perfil.setText(contaAdministradora.getNome_usuario_instagram());
+            }
+            if (contaAdministradora.getNome_usuario_instagram() == null)
+            {
+
+                tv_facebook_salao_perfil.setText("Não cadastrado :/");
+            }
+            else
+            {
+                tv_facebook_salao_perfil.setText(contaAdministradora.getLink_facebook());
+            }
+
+
+        //FORMAS DE PAGAMENTO
+            int contador = 0;
+
+            while (contador < contaAdministradora.getDados_formas_pagamento().size())
+            {
+                String tag = "tv_forma_pagto_" + contaAdministradora.getDados_formas_pagamento().get(contador);
+                TextView txt_forma_pagamento = ln_container_formas_pagamento_perfil.findViewWithTag(tag);
+
+                txt_forma_pagamento.setVisibility(View.VISIBLE);
+
+                contador= contador+1;
+            }
+
+
+        //FUNCIONAMENTO
+            //pegar os lineares pelo id container_geral_dos_horarios
+            //pegar os elementos pelo tag
+
+        //OBSERVACOES DE PAGAMENTO
+        //REGRAS DE NEGÓCIO
+
+
+
     }
 }
