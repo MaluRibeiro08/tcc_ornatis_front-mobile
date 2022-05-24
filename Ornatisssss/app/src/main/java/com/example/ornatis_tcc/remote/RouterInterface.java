435e4330316ad35f6ab6c68eaf21cff0f4a388a7
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-05-24 11:35:29 -0300

    Reorganização das rotas

--- Ornatisssss/app/src/main/java/com/example/ornatis_tcc/remote/RouterInterface.java
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/remote/RouterInterface.java
@@ -15,37 +15,42 @@ import retrofit2.http.Path;
 
 public interface RouterInterface {
 
-    //INSERÇÃO DE CONTA
-    @POST("/contaAdministradora/cadastrarConta")//rota da req e o verbo http
-    Call<ContaAdministradora> addEmpresa(@Body ContaAdministradora objUsuario);
+//ADM
+    //CONTA ADM
+        //INSERÇÃO DE CONTA
+        @POST("/contaAdministradora/cadastrarConta")//rota da req e o verbo http
+        Call<ContaAdministradora> addEmpresa(@Body ContaAdministradora objUsuario);
 
-    //LISTAGEM DE DADOS DE PERFIL
-    @GET("/contaAdministradora/listarPerfil/{id_empresa}")
-    Call<ContaAdministradora> getDadosPerfilEstabelecimento(@Path("id_empresa") int id_empresa);
+        //LISTAGEM DE DADOS DE PERFIL
+        @GET("/contaAdministradora/listarPerfil/{id_empresa}")
+        Call<ContaAdministradora> getDadosPerfilEstabelecimento(@Path("id_empresa") int id_empresa);
 
-    //INSERÇÃO DE FUNCIONÁRIO
-    @POST("/contaAdministradora/cadastroFuncionario")
-    Call<Funcionario> addFuncionario(@Body Funcionario objFuncionario);
+    //FUNCIONARIOS
+        //INSERÇÃO DE FUNCIONÁRIO
+        @POST("/contaAdministradora/cadastroFuncionario")
+        Call<Funcionario> addFuncionario(@Body Funcionario objFuncionario);
 
-    //LISTAGEM DE FUNCIONÁRIOS
-    @GET("/contaAdministradora/listarFuncionarios/{id_empresa}")
-    Call<ArrayList<Funcionario>> getFuncionarios(@Path("id_empresa") int id_empresa);
+        //LISTAGEM DE FUNCIONÁRIOS
+        @GET("/contaAdministradora/listarFuncionarios/{id_empresa}")
+        Call<ArrayList<Funcionario>> getFuncionarios(@Path("id_empresa") int id_empresa);
 
-    //EXCLUSÃO DE FUNCIONÁRIO
-//    @DELETE("/contaAdministradora/excluirFuncionario")
-//    Call<Funcionario> deleteFuncionario(@Body Funcionario funcionario);
+        //EXCLUSÃO DE FUNCIONÁRIO
+        @DELETE("/contaAdministradora/desabilitarFuncionario/{id_funcionario}")
+        Call<Funcionario> deleteFuncionario(@Path("id_funcionario") int id_funcionario);
 
+    //SERVIÇOS
+        //INSERÇÃO DE SERVIÇO
+        @POST("/contaAdministradora/cadastrarServico")
+        Call<Servico> addServico(@Body Servico objServico);
 
-    @DELETE("/contaAdministradora/desabilitarFuncionario/{id_funcionario}")
-    Call<Funcionario> deleteFuncionario(@Path("id_funcionario") int id_funcionario);
+        //LISTAGEM DE SERVIÇOS
+        @GET("/contaAdministradora/listarServicos/{id_empresa}")
+        Call<ArrayList<Servico>> getServicos(@Path("id_empresa") int id_empresa);
 
-    //INSERÇÃO DE SERVIÇO
-    @POST("/contaAdministradora/cadastrarServico")
-    Call<Servico> addServico(@Body Servico objServico);
+        //EXCLUSÃO DE SERVIÇOS
+        @DELETE("contaAdministradora/desabilitarServico/{id_servico}")
+        Call<Servico> deleteServico(@Path("id_servico") int id_servico);
 
-    //LISTAGEM DE SERVIÇOS
-    @GET("/contaAdministradora/listarServicos/{id_empresa}")
-    Call<ArrayList<Servico>> getServicos(@Path("id_empresa") int id_empresa);
 
 
     //DESATUALIZADAS
