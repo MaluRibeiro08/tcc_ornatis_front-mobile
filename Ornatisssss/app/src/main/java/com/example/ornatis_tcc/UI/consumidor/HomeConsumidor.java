64d19a84f3106d742da9c9a9ce4b0b0a212e3a6b
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-05-27 11:39:24 -0300

    Criação tela home de consumidor

---
+++ Ornatisssss/app/src/main/java/com/example/ornatis_tcc/UI/consumidor/HomeConsumidor.java
@@ -0,0 +1,16 @@
+package com.example.ornatis_tcc.UI.consumidor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ornatis_tcc.R;
+
+public class HomeConsumidor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_consumidor);
+    }
+}
